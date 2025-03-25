d14da78fe87a59d14d740168c46074568c419939
Javier Jimenez <869756@unizar.es>
2025-03-25 17:36:20 +0100

    Implementacion backbutton logros

--- src/components/Logros.jsx
+++ src/components/Logros.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import NavBarGame from "./layout/game/NavbarGame"; // Asegúrate de importar correctamente los componentes
 import background from "../assets/background.png";
 import BackButton from "./layout/game/BackButton";
 import AchievementList from "./layout/game/AchievementList"; // Importamos AchievementList
 
 const Logros = () => {
+  const navigate = useNavigate();
+  
+  const handleBackClick = () => {
+    navigate("/home");
+  };
+  
   return (
     <div
       className="fixed inset-0 flex justify-center items-center bg-cover bg-center"
@@ -15,7 +22,7 @@ const Logros = () => {
       <div className="relative h-screen w-full flex items-center px-64">
         {/* Botón de volver */}
         <div className="absolute left-10 top-10 mt-16">
-          <BackButton />
+          <BackButton onClick={handleBackClick}/>
         </div>
 
         {/* Lista de logros */}
